a3261b7ac56f4f032e1089e0af2c30936f00ab77
lynda-nneka <86114880+lynda-nneka@users.noreply.github.com>
2022-11-17 09:27:17 +0100

    Add files via upload

---
+++ pomodorotimer/PomodoroTimer.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Timer = System.Timers;
+
+public class PomodoroTimer
+{
+    public int totalWorkTimeInSeconds = 1 * 60;
+    public int totalRestTimeInSeconds = 1 * 60;
+    public Timer.Timer mainTimer = new Timer.Timer();
+    public Timer.Timer restTimer = new Timer.Timer();
+
+    public void Run()
+    {
+        Console.WriteLine("What task do you want to work on");
+
+        string userTask = Console.ReadLine();
+
+        SetupTimer();
+        StartWorkTimer();
+
+    }
+
+    private void SetupTimer()
+    {
+        mainTimer.Interval = 1000;
+        mainTimer.Elapsed += OnWorkTimerElapsedEvent;
+
+        restTimer.Interval = 1000;
+        restTimer.Elapsed += OnRestTimerElapsedEvent;
+    }
+
+    private void StartWorkTimer()
+    {
+
+        mainTimer.Start();
+
+    }
+
+    private void StartRestTimer()
+    {
+        Console.WriteLine("Take a short break (5 minutes)");
+        restTimer.Start();
+    }
+
+    private void ResetTimer()
+    {
+        totalWorkTimeInSeconds = 1 * 60;
+        totalRestTimeInSeconds = 1 * 60;
+
+        mainTimer.Start();
+    }
+
+    private void OnWorkTimerElapsedEvent(Object source, Timer.ElapsedEventArgs e)
+    {
+        totalWorkTimeInSeconds--;
+        TimeSpan time = TimeSpan.FromSeconds(totalWorkTimeInSeconds);
+        string timeMessage = time.ToString(@"mm\:ss");
+
+        Console.WriteLine("Work Time Remaining " + timeMessage);
+
+        if (totalWorkTimeInSeconds <= 0)
+        {
+            mainTimer.Stop();
+            Console.WriteLine("Time is up!");
+            StartRestTimer();
+        }
+    }
+
+    private void OnRestTimerElapsedEvent(Object source, Timer.ElapsedEventArgs e)
+    {
+        totalRestTimeInSeconds--;
+        Console.WriteLine("Rest Time Passed " + totalRestTimeInSeconds);
+
+        if (totalRestTimeInSeconds <= 0)
+        {
+            restTimer.Stop();
+            Console.WriteLine("Rest Time is up!");
+            ResetTimer();
+        }
+    }
+}
+
---
+++ pomodorotimer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Timer = System.Timers;
+
+public class Program
+{
+    public static int totalWorkTimeInSeconds = 25 * 60;
+    public static int totalRestTimeInSeconds = 5 * 60;
+    public static Timer.Timer mainTimer = new Timer.Timer();
+    public static Timer.Timer restTimer = new Timer.Timer();
+
+    public static void Main()
+    {
+        Console.WriteLine("press Q to exit the program");
+
+        PomodoroTimer pomodoroTimer = new PomodoroTimer();
+        pomodoroTimer.Run();
+
+
+
+
+        while (Console.ReadKey().Key != ConsoleKey.Q) { }
+    }
+
+}
